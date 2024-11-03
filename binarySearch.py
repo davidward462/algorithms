ac7e4f8554b98d2fe987d17bcb200bec0f33e28d
David Ward <davidward462@users.noreply.github.com>
2024-11-02 23:42:44 -0700

    move test code out of binary search file

--- binarySearch.py
+++ binarySearch.py
@@ -1,5 +1,3 @@
-import pytest
-
 def binarySearch(data, item):
 
         # start of the list
@@ -10,20 +8,17 @@ def binarySearch(data, item):
         while (low <= high):
 
                 # check the middle element
-                mid = (low + high) / 2 # rounded down
+                mid = (low + high) // 2 # rounded down
                 guess = data[mid]
+
                 if (guess == item):
                         # found
                         return mid
-                if (guess > item):
+                elif (guess > item):
                         # too high
                         high = mid - 1
-                if (guess > item):
+                else:
                         # too low
                         low = mid + 1
-                return None
-
-def test_binarySearch():
-        assert binarySearch([1, 2, 3, 4, 6, 29, 100], 100) == 6
-        assert binarySearch([23], 23) == 0
-        assert binarySearch([-3, 0, 3, 6, 9, 10, 12, 50, 55, 69], 3) == 2
+        # if pointers meet or pass, we did not find the item
+        return None
