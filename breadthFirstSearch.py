73adf2681d3b6b21c141e14c41f332d9fbd4b664
David Ward <davidward462@users.noreply.github.com>
2024-11-09 14:36:20 -0800

    comment bfs

--- breadthFirstSearch.py
+++ breadthFirstSearch.py
@@ -1,15 +1,23 @@
 from collections import deque
 
 def breadthFirstSearch(graph, start, search_key):
+        # create queue
         search_queue = deque()
+
+        # get startng node
         search_queue += graph[start]
 
         # while queue is not empty
         while search_queue:
-                node = search_queue.popleft() #dequeue
+
+                # dequeue node
+                node = search_queue.popleft()
+
+                # check search condition here
                 if (node == search_key):
                         return True
                 else:
+                        # if not found, enqueue the node's neighbours
                         search_queue += graph[node]
         # not found
         return False
