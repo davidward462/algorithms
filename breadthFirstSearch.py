621b283ab408e4aaffb038d59c6003ad0e0042fc
David Ward <davidward462@users.noreply.github.com>
2024-11-09 15:00:49 -0800

    keep track of visited nodes

--- breadthFirstSearch.py
+++ breadthFirstSearch.py
@@ -3,6 +3,7 @@ from collections import deque
 def breadthFirstSearch(graph, start, search_key):
         # create queue
         search_queue = deque()
+        visited = []
 
         # get startng node
         search_queue += graph[start]
@@ -13,12 +14,16 @@ def breadthFirstSearch(graph, start, search_key):
                 # dequeue node
                 node = search_queue.popleft()
 
-                # check search condition here
-                if (node == search_key):
-                        return True
+                # if node has not been visited
+                if node not in visited:
+
+                        # check search condition here
+                        if (node == search_key):
+                                return True
                 else:
                         # if not found, enqueue the node's neighbours
                         search_queue += graph[node]
+                        visited.append(node)
         # not found
         return False
 
